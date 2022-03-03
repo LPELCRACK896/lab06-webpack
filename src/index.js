6c52a755eab516483393edc42ccb5a1b1460d9d3
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-03-03 03:41:46 -0600

    SASS y Bootstrap loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { startApp } from "./app/src/app";
 import './CSS/index_style.css'
+import  'bootstrap'
 startApp()
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.s[as]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     }
